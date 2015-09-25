69115ae907e7306cba9fad0bb15a3e154f5c1c57
Harish <harishprofile001@gmail.com>
2015-09-25 12:33:50 +0530

    committed by me

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ module.exports = function(flights, db) {
   var app = express();
   
   // all environments
-  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+  //app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   app.use(express.favicon());
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var http = require('http'),
     flights = require('./data'),
     db = require('./db'),
     repl = require("repl"),
-    app = require('./app')(flights, db);
+    app = require('./app')(flights, db),
+    port = process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(port, ip, function() {
+  console.log('Express server listening on port ' + port);
 });
 
 var prompt = repl.start({prompt: "airline> "});
